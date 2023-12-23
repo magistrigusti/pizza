5184b5c89e9a6a9d66c008c666d904dae23db20d
Magistry <Magistrigusti@yandex.ru>
2023-12-23 14:05:37 +0200

    0.01

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import Header from './Header';
+import { Header, Categories } from './components';
 
 
 function App() {
+
   return (
     <div className="App">
       <div className="wrapper">
-      
       <Header />
-
+  
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            
+            <Categories items={[
+              'Мясные',
+              'Вегетарианская',
+              'Гриль',
+              'Острые',
+              'Закрытые'
+            ]} />
+
             <div className="sort">
               <div className="sort__label">
                 <svg

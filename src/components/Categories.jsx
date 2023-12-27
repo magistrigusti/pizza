cdf364f45ee7f5b8650b24bf844cc42f01e60eda
Magistry <Magistrigusti@yandex.ru>
2023-12-27 07:07:25 +0200

    0.02

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-function Categories ({ items }) {
-  return (
-    <div className='categories'>
+function Categories ({items}) {
+  const [activeItem, setActiveItem] = React.useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  }
+
+  return(
+    <div className="categories">
       <ul>
-        <li className="active">All</li>
-        {
+        <li className={activeItem === null ? 'active' : ''}
+          onClick={() => onSelectItem(null)}
+        >Все</li>
+        {items &&
           items.map((name, index) => (
-            <li key={`${name}_${index}`}>{name}</li>
+            <li className={activeItem === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}
+            >{name}</li>
           ))
         }
       </ul>
